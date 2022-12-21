e878eddff1c1ad7c97c885f4eecab6ea602ac89d
Matias <mporra.ie@gmail.com>
2022-12-21 03:05:54 -0300

    omiting our dataset

--- 01_training.py
+++ 01_training.py
@@ -44,39 +44,50 @@ if len(list_mlflow_experiments):
     mlflow.create_experiment(str(new_experiment_id))
 
 
-# This should be a query from a database
-DATASET_PATH = '/home/ml2/data_playlist.csv'
-df = pd.read_csv(DATASET_PATH, delimiter=',')
-X = df.drop(['label'],axis = 1)
-y = df['label']
+# # This should be a query from a database
+# DATASET_PATH = '/home/ml2/data_playlist.csv'
+# df = pd.read_csv(DATASET_PATH, delimiter=',')
+# X = df.drop(['label'],axis = 1)
+# y = df['label']
 
-# preparo el dataset
-#creamos mapping para TEMPO, para transformarlo en categorica y mappeamos a binarios
-tempo_mappings = {
-    (40,60)   : '000', #'lento',
-    (60,66)   : '001', #'Larghetto',
-    (66,76)   : '010', #'Adagio',
-    (76,108)  : '011', #'Andante',
-    (108,120) : '100', #'Moderato',
-    (120,168) : '101', #'Allegro',
-    (168,200) : '110', #'Presto',
-    (200,216) : '111', #'Prestissimo',
-                }
+# # preparo el dataset
+# #creamos mapping para TEMPO, para transformarlo en categorica y mappeamos a binarios
+# tempo_mappings = {
+#     (40,60)   : '000', #'lento',
+#     (60,66)   : '001', #'Larghetto',
+#     (66,76)   : '010', #'Adagio',
+#     (76,108)  : '011', #'Andante',
+#     (108,120) : '100', #'Moderato',
+#     (120,168) : '101', #'Allegro',
+#     (168,200) : '110', #'Presto',
+#     (200,216) : '111', #'Prestissimo',
+#                 }
 
 
-def map_tempos(x):
-    for key in tempo_mappings:
-        if x >= key[0] and x <= key[1]:
-            return tempo_mappings[key]
+# def map_tempos(x):
+#     for key in tempo_mappings:
+#         if x >= key[0] and x <= key[1]:
+#             return tempo_mappings[key]
 
-df['tempo'] = df['tempo'].apply(map_tempos)
+# df['tempo'] = df['tempo'].apply(map_tempos)
 
-#lo mismo para liveness
-df['liveness'] = df['liveness'].apply(lambda d: 1 if d>0.8 else 0)
+# #lo mismo para liveness
+# df['liveness'] = df['liveness'].apply(lambda d: 1 if d>0.8 else 0)
 
-mlflow.sklearn.autolog(max_tuning_runs = None)
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.1,random_state=2022)
+# mlflow.sklearn.autolog(max_tuning_runs = None)
+# X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.1,random_state=2022)
  
+ 
+### POR AHORA VAMOS A USAR EL DATASET DE TEST HASTA SOLUCIONAR EL PROBLEMA DEL NUESTRO
+
+# This should be a query from a database
+X = sklearn.datasets.load_iris().data
+y = sklearn.datasets.load_iris().target
+
+# # Usando el autolog muchas metricas se guardan solas...
+mlflow.sklearn.autolog(max_tuning_runs = None)
+X_train, X_test, y_train, y_test = train_test_split(X,y)
+
 
 def log_model(model,
               developer = None,
