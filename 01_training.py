4322a998c08962cef21d8cd70e92afde27ae7e73
Matias <mporra.ie@gmail.com>
2022-12-21 03:33:10 -0300

    Update 01_training.py

--- 01_training.py
+++ 01_training.py
@@ -8,16 +8,9 @@ import mlflow
 from mlflow.client import MlflowClient
 
 import os
-import sklearn
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.svm import SVC
-from sklearn.linear_model import LogisticRegression
+import sklearn 
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-import numpy  as np
-from numpy import random as rd
-from matplotlib import pyplot as plt
-
+from sklearn.model_selection import train_test_split 
 
 from sklearn import svm, datasets
 from sklearn.model_selection import GridSearchCV
